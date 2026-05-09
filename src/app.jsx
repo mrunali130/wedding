2707bfe62685bddefb6f31e7a01e180b15f1d7bc
mrunali130 <mrunali1309@gmail.com>
2026-05-09 14:35:13 +0530

    photo of couple

--- src/app.jsx
+++ src/app.jsx
@@ -9,7 +9,7 @@ const WEDDING_CONFIG = {
   venue: "Symphony Banquet Hall",
   address: "Near Vijaynagar Society, Nityanand Marg, Andheri East, Mumbai, Maharashtra 400069",
   hostedBy: "Tamhankars & Godboles",
-  coupleImageSrc: "public\couple.png",
+  coupleImageSrc: "public\couples.png",
   ganeshImageSrc: "/ganesh.png",
 };
 
